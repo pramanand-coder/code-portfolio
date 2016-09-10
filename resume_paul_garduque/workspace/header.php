01211be6f806f949f76de03ec8573da1e805af28
pgarduque <pgarduque@yahoo.com>
2016-09-09 21:21:46 -0700

    Add files via upload

---
+++ resume_paul_garduque/workspace/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<body>
+    <div id="header" class="container">
+        <div id="logo">
+        <h1><a href="index.php"style="width:0px;height:0px;background-color:#FFFFFF"><img src="img/logo.jpg"/></a></h1>
+        </div>
+    </div>
+</body>
+</html>
